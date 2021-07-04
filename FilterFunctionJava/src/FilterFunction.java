27855f6661a01471c86cc1a0e710609328caffb9
gabriele grieco <gabrielegriecomusician@gmail.com>
2021-07-04 16:43:36 +0200

    PushingMyTips

--- FilterFunctionJava/src/FilterFunction.java
+++ FilterFunctionJava/src/FilterFunction.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class FilterFunction {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class FilterFunction {
+    static void main (String[] args){
+        List<String> items = new ArrayList<String>();
+        items.add("one");
+        items.add("two");
+        items.add("three");
+
+        Stream<String> stream = items.stream();
+        stream.filter( item ->  item.startsWith("o") );
+    }
 }
